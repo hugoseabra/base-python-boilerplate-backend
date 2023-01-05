1bd08a69835558482716fe6a255e56c76611adfb
Hugo Seabra <hugoseabra19@gmail.com>
2023-01-05 09:50:03 -0300

    Changing approach on how to handle docker-compose services

--- conf/docker/services/migration.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-source /code/deploy/docker/scripts/runner.sh
-
-run_python_script_with_output "Executando migrate" "manage.py migrate"
-run_python_script_with_output "Criado tabelas de cache" "manage.py createcachetable"
